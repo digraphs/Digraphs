4fa763737c97763c98b2223f8fbfe5d2f6e06407
Julius <unknown>
2014-12-10 14:17:20 +0000

    getting rid of 8 in homes.c. jj

--- src/homos.c
+++ src/homos.c
@@ -45,7 +45,7 @@ static inline UIntS sizeUIntL (UIntL n, int m) {
 
 HomosGraph* new_homos_graph (UIntS const nr_verts) {
   HomosGraph* graph = malloc(sizeof(HomosGraph));
-  graph->neighbours = calloc(8 * nr_verts, sizeof(UIntL));
+  graph->neighbours = calloc(((nr_verts - 1) / SYS_BITS + 1) * nr_verts, sizeof(UIntL));
   graph->nr_verts = nr_verts;
   inittabs();
   return graph;
@@ -58,7 +58,7 @@ void free_homos_graph (HomosGraph* graph) {
 }
 
 void add_edges_homos_graph (HomosGraph* graph, UIntS from_vert, UIntS to_vert) {
-  graph->neighbours[8 * from_vert + (to_vert / SYS_BITS)] |= oneone[to_vert % SYS_BITS];
+  graph->neighbours[((graph->nr_verts - 1) / SYS_BITS + 1) * from_vert + (to_vert / SYS_BITS)] |= oneone[to_vert % SYS_BITS];
 }
 
 // automorphism group 
@@ -66,6 +66,7 @@ void add_edges_homos_graph (HomosGraph* graph, UIntS from_vert, UIntS to_vert) {
 static BlissGraph* as_bliss_graph (HomosGraph* graph) {
   UIntS  i, j, k;
   UIntS  n = graph->nr_verts;
+  UIntS  d = (graph->nr_verts - 1 / SYS_BITS) + 1;
   UIntS  m = (n / SYS_BITS);
   
   BlissGraph* bliss_graph = bliss_new(n);
@@ -73,13 +74,13 @@ static BlissGraph* as_bliss_graph (HomosGraph* graph) {
   for (i = 0; i < n; i++) {   // loop over vertices
     for (j = 0; j < m; j++) { // loop over neighbours of vertex <i>
       for (k = 0; k < SYS_BITS; k++) {
-        if (graph->neighbours[8 * i + j] & oneone[k]) {
+        if (graph->neighbours[d * i + j] & oneone[k]) {
           bliss_add_edge(bliss_graph, i, SYS_BITS * j + k);
         }
       }
     }
     for (k = 0; k < (n % SYS_BITS); k++) {
-      if (graph->neighbours[8 * i + j] & oneone[k]) {
+      if (graph->neighbours[d * i + j] & oneone[k]) {
         bliss_add_edge(bliss_graph, i, SYS_BITS * j + k);
       }
     }
@@ -116,6 +117,8 @@ static UIntS  nr1;             // nr of vertices in graph1
 static UIntS  nr2;             // nr of vertices in graph2
 static UIntS  nr2_d;           // nr2 / SYS_BITS 
 static UIntS  nr2_m;           // nr2 % SYS_BITS 
+static UIntS  len_nr1;         // number of UIntS to store all neighbours1
+static UIntS  len_nr2;         // number of UIntS to store all neighbours2
 static UIntL  count;                   // the UIntLber of endos found so far
 static UIntS  hint;           // an upper bound for the UIntLber of distinct values in map
 static UIntL  maxresults;              // upper bound for the UIntLber of returned homos
@@ -123,12 +126,12 @@ static UIntS  map[MAXVERTS];                 // partial image list
 static UIntS  sizes[MAXVERTS * MAXVERTS];  // sizes[depth * nr1 + i] = |condition[i]| at depth <depth>
 static UIntS  orb[MAXVERTS];                 // to hold the orbits in OrbitReps
 
-static UIntL   vals[8];                 // blist for values in map
-static UIntL   neighbours1[8 * MAXVERTS];      // the neighbours of the graph1
-static UIntL   neighbours2[8 * MAXVERTS];      // the neighbours of the graph2
-static UIntL   domain[8];                     // for finding orbit reps               
-static UIntL   orb_lookup[8];                 // for finding orbit reps
-static UIntL   reps[8 * MAXVERTS];
+static UIntL   vals[MAXVERTS / SYS_BITS];                 // blist for values in map
+static UIntL   neighbours1[(MAXVERTS / SYS_BITS) * MAXVERTS];      // the neighbours of the graph1
+static UIntL   neighbours2[(MAXVERTS / SYS_BITS) * MAXVERTS];      // the neighbours of the graph2
+static UIntL   domain[MAXVERTS / SYS_BITS];                     // for finding orbit reps               
+static UIntL   orb_lookup[MAXVERTS / SYS_BITS];                 // for finding orbit reps
+static UIntL   reps[(MAXVERTS / SYS_BITS) * MAXVERTS];
 
 static void*  user_param;              // a user_param for the hook
 void         (*hook)(void*        user_param,
@@ -165,13 +168,13 @@ static void orbit_reps (UIntS rep_depth) {
   UIntS     nrgens, i, j, fst, m, img, n, max, d;
   Perm      gen;
  
-  for (i = 8 * rep_depth; i < 8 * (rep_depth + 1); i++) {
+  for (i = len_nr2 * rep_depth; i < len_nr2 * (rep_depth + 1); i++) {
     reps[i] = 0;
   }
 
   // TODO special case in case there are no gens, or just the identity.
 
-  for (i = 0; i < 8; i++){
+  for (i = 0; i < len_nr2; i++){
     domain[i] = 0;
     orb_lookup[i] = 0;
   }
@@ -190,7 +193,7 @@ static void orbit_reps (UIntS rep_depth) {
   while (fst < deg) {
     d = fst / SYS_BITS;
     m = fst % SYS_BITS;
-    reps[(8 * rep_depth) + d] |= oneone[m];
+    reps[(len_nr2 * rep_depth) + d] |= oneone[m];
     orb[0] = fst;
     n = 1;   //length of orb
     orb_lookup[d] |= oneone[m];
@@ -221,7 +224,7 @@ void SEARCH_HOMOS_SM (UIntS depth,        // the number of filled positions in m
                       UIntS   rank){      // current number of distinct values in map
 
   UIntS  i, j, k, l, min, next, m, sum, w;
-  UIntL  copy[8 * nr1];
+  UIntL  copy[len_nr2 * nr1];
   
   calls1++;
   if (calls1 > last_report + report_interval) {
@@ -244,7 +247,7 @@ void SEARCH_HOMOS_SM (UIntS depth,        // the number of filled positions in m
     return;
   }
 
-  memcpy((void *) copy, (void *) condition, (size_t) nr1 * 8 * sizeof(UIntL));
+  memcpy((void *) copy, (void *) condition, (size_t) nr1 * len_nr2 * sizeof(UIntL));
   next = 0;      // the next position to fill
   min = nr2 + 1; // the minimum number of candidates for map[next]
 
@@ -253,14 +256,14 @@ void SEARCH_HOMOS_SM (UIntS depth,        // the number of filled positions in m
       i = j / SYS_BITS;
       m = j % SYS_BITS;
       if (map[j] == UNDEFINED) {
-        if (neighbours1[pos * 8 + i] & oneone[m]) { // vertex j is adjacent to vertex pos in graph1
+        if (neighbours1[pos * len_nr1 + i] & oneone[m]) { // vertex j is adjacent to vertex pos in graph1
           sizes[depth * nr1 + j] = 0;
 	  for (k = 0; k < nr2_d; k++){
-            copy[8 * j + k] &= neighbours2[8 * map[pos] + k];
-            sizes[depth * nr1 + j] += sizeUIntL(copy[8 * j + k], SYS_BITS);
+            copy[len_nr2 * j + k] &= neighbours2[len_nr2 * map[pos] + k];
+            sizes[depth * nr1 + j] += sizeUIntL(copy[len_nr2 * j + k], SYS_BITS);
 	  }
-          copy[8 * j + nr2_d] &= neighbours2[8 * map[pos] + nr2_d];
-          sizes[depth * nr1 + j] += sizeUIntL(copy[8 * j + nr2_d], nr2_m);
+          copy[len_nr2 * j + nr2_d] &= neighbours2[len_nr2 * map[pos] + nr2_d];
+          sizes[depth * nr1 + j] += sizeUIntL(copy[len_nr2 * j + nr2_d], nr2_m);
           if (sizes[depth * nr1 + j] == 0) {
             return;
           }
@@ -280,7 +283,7 @@ void SEARCH_HOMOS_SM (UIntS depth,        // the number of filled positions in m
     for (i = 0; i < nr2; i++) {
       j = i / SYS_BITS;
       m = i % SYS_BITS;
-      if ((copy[8 * next + j] & reps[(8 * rep_depth) + j] & oneone[m]) 
+      if ((copy[len_nr2 * next + j] & reps[(len_nr2 * rep_depth) + j] & oneone[m]) 
           && (vals[j] & oneone[m]) == 0) { 
         calls2++;
 
@@ -299,7 +302,7 @@ void SEARCH_HOMOS_SM (UIntS depth,        // the number of filled positions in m
   for (i = 0; i < nr2; i++) {
     j = i / SYS_BITS;
     m = i % SYS_BITS;
-    if (copy[8 * next + j] & vals[j] & oneone[m]) {
+    if (copy[len_nr2 * next + j] & vals[j] & oneone[m]) {
       map[next] = i;
       SEARCH_HOMOS_SM(depth + 1, next, copy, rep_depth, rank);
       map[next] = UNDEFINED;
@@ -323,6 +326,8 @@ void GraphHomomorphisms (HomosGraph*  graph1,
   
   nr1 = graph1->nr_verts;
   nr2 = graph2->nr_verts;
+  len_nr1 = (nr1 - 1) / SYS_BITS + 1;
+  len_nr2 = (nr2 - 1) / SYS_BITS + 1;
   nr2_d = nr2 / SYS_BITS;
   nr2_m = nr2 % SYS_BITS;
 
@@ -335,24 +340,24 @@ void GraphHomomorphisms (HomosGraph*  graph1,
   // initialise everything . . .
   inittabs();
   
-  UIntL condition[8 * nr1];
+  UIntL condition[len_nr2 * nr1];
   d = nr1 / SYS_BITS;
   m = nr1 % SYS_BITS;
   for (i = 0; i < nr1; i++) {
     map[i] = UNDEFINED;
     sizes[i] = nr2;
     for (j = 0; j < d; j++){
-      condition[8 * i + j] = ones[63];
+      condition[len_nr2 * i + j] = ones[63];
     }
-    condition[8 * i + d] = ones[m];
+    condition[len_nr2 * i + d] = ones[m];
   }
   
-  for (i = 0; i < 8; i++){
+  for (i = 0; i < len_nr2; i++){
     vals[i] = 0;
   }
 
-  memcpy((void *) neighbours1, graph1->neighbours, nr1 * 8 * sizeof(UIntL));
-  memcpy((void *) neighbours2, graph2->neighbours, nr2 * 8 * sizeof(UIntL));
+  memcpy((void *) neighbours1, graph1->neighbours, nr1 * len_nr1 * sizeof(UIntL));
+  memcpy((void *) neighbours2, graph2->neighbours, nr2 * len_nr2 * sizeof(UIntL));
 
   // get generators of the automorphism group
   set_perms_degree(nr2);
