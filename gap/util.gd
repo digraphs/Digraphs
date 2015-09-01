28b8ad089c96fbbd1305a7053a10cc7edf8641e3
Wilf Wilson <wilf@wilf-wilson.net>
2015-09-01 16:49:35 +0100

    Binding DIGRAPHS_OmitFromTestManualExamples for Digraphs can be loaded without GRAPE. WW

--- gap/util.gd
+++ gap/util.gd
@@ -9,6 +9,7 @@
 ##
 
 BindGlobal("DIGRAPHS_OmitFromTests", []);
+BindGlobal("DIGRAPHS_OmitFromTestManualExamples", []);
 
 DeclareGlobalFunction("DIGRAPHS_Dir");
 DeclareGlobalFunction("DIGRAPHS_Test");
