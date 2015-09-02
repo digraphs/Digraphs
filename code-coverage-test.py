0237bc026b6742c62080e083fc55fd7fec88d27e
Wilf Wilson <wilf@wilf-wilson.net>
2015-09-02 10:57:13 +0100

    Making the code coverage tool not print out lots of gap> things. WW

--- code-coverage-test.py
+++ code-coverage-test.py
@@ -24,7 +24,7 @@ OutputAnnotatedCodeCoverageFiles(x, filesdir, outdir);"'''
 PS = subprocess.Popen(COMMANDS, stdout=subprocess.PIPE, shell=True)
 
 try:
-    subprocess.check_call('~/gap/bin/gap.sh -A -r -m 1g -T',
+    subprocess.check_call('~/gap/bin/gap.sh -A -r -q -m 1g -T',
                           stdin=PS.stdout, shell=True)
 except subprocess.CalledProcessError:
     sys.exit('code-coverage-test: something went wrong calling GAP!')
