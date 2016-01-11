1d9e901a391257334ad6a1a1b5d55337791e8bad
Michael Torpey <mct25@st-andrews.ac.uk>
2016-01-11 15:55:13 +0000

    Fixed codeship problems

--- scripts/codeship.sh
+++ scripts/codeship.sh
@@ -2,36 +2,36 @@ echo "=========================================================="
 echo "GAP DEV: Running tests with Semigroups and Grape loaded..."
 echo "=========================================================="
 echo "LoadPackage(\"digraphs\"); LoadPackage(\"semigroups\"); DigraphsTestAll(); quit;" | sh gap/bin/gap.sh | tee testlog.txt
-echo "==========================================="
-echo "GAP DEV: Running tests with Grape loaded..."
-echo "==========================================="
+echo "================================================================"
+echo "GAP DEV: Running tests with Grape loaded (but NOT Semigroups)..."
+echo "================================================================"
 echo "LoadPackage(\"digraphs\"); DigraphsTestAll(); quit;" | sh gap/bin/gap.sh | tee -a testlog.txt
-rm -r pkg/grape
-echo "================================================"
-echo "GAP DEV: Running tests with Semigroups loaded..."
-echo "================================================"
-echo "LoadPackage(\"digraphs\"); DigraphsTestAll(); quit;" | sh gap/bin/gap.sh | tee -a testlog.txt
-echo "=========================================================="
-echo "GAP DEV: Running tests with no optional packages loaded..."
-echo "=========================================================="
+rm -r gap/pkg/grape
+echo "================================================================"
+echo "GAP DEV: Running tests with Semigroups loaded (but NOT Grape)..."
+echo "================================================================"
 echo "LoadPackage(\"digraphs\"); LoadPackage(\"semigroups\"); DigraphsTestAll(); quit;" | sh gap/bin/gap.sh | tee -a testlog.txt
+echo "============================================================"
+echo "GAP DEV: Running tests WITHOUT Semigroups or Grape loaded..."
+echo "============================================================"
+echo "LoadPackage(\"digraphs\"); DigraphsTestAll(); quit;" | sh gap/bin/gap.sh | tee -a testlog.txt
 
 echo "=========================================================="
 echo "GAP 4.8: Running tests with Semigroups and Grape loaded..."
 echo "=========================================================="
 echo "LoadPackage(\"digraphs\"); LoadPackage(\"semigroups\"); DigraphsTestAll(); quit;" | sh gap4r8/bin/gap.sh | tee -a testlog.txt
-echo "==========================================="
-echo "GAP 4.8: Running tests with Grape loaded..."
-echo "==========================================="
+echo "================================================================"
+echo "GAP 4.8: Running tests with Grape loaded (but NOT Semigroups)..."
+echo "================================================================"
 echo "LoadPackage(\"digraphs\"); DigraphsTestAll(); quit;" | sh gap4r8/bin/gap.sh | tee -a testlog.txt
-rm -r pkg/grape
-echo "================================================"
-echo "GAP 4.8: Running tests with Semigroups loaded..."
-echo "================================================"
-echo "LoadPackage(\"digraphs\"); DigraphsTestAll(); quit;" | sh gap4r8/bin/gap.sh | tee -a testlog.txt
-echo "=========================================================="
-echo "GAP 4.8: Running tests with no optional packages loaded..."
-echo "=========================================================="
+rm -r gap4r8/pkg/grape
+echo "================================================================"
+echo "GAP 4.8: Running tests with Semigroups loaded (but NOT Grape)..."
+echo "================================================================"
 echo "LoadPackage(\"digraphs\"); LoadPackage(\"semigroups\"); DigraphsTestAll(); quit;" | sh gap4r8/bin/gap.sh | tee -a testlog.txt
+echo "============================================================"
+echo "GAP 4.8: Running tests WITHOUT Semigroups or Grape loaded..."
+echo "============================================================"
+echo "LoadPackage(\"digraphs\"); DigraphsTestAll(); quit;" | sh gap4r8/bin/gap.sh | tee -a testlog.txt
 
 grep --colour=always -A 1 -E "########> Diff|$" testlog.txt ; ( ! grep -E "########> Diff" testlog.txt )
