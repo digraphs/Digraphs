b074d8f82faa823b11dcef951573712864a175ed
Michael Torpey <mct25@st-andrews.ac.uk>
2016-02-08 12:40:33 +0000

    codeship now tries to rebuild Digraphs, not GAP

--- scripts/codeship.sh
+++ scripts/codeship.sh
@@ -15,9 +15,11 @@ echo "GAP DEV: Running tests with Grape loaded (but NOT Semigroups)..."
 echo "================================================================"
 echo "LoadPackage(\"digraphs\"); DigraphsTestAll(); quit;" | sh gap/bin/gap.sh | tee -a testlog.txt
 
+cd gap/pkg/digraphs
 make clean
 ./configure CFLAGS=-m32 CXXFLAGS=-m32 LDFLAGS=-m32
 make
+cd ../../..
 echo "==================================================================="
 echo "GAP DEV (32-bit): Running tests with Semigroups and Grape loaded..."
 echo "==================================================================="
@@ -37,9 +39,11 @@ echo "LoadPackage(\"digraphs\"); DigraphsTestAll(); quit;" | sh gap/bin/gap.sh |
 
 rm -r gap/pkg/grape
 
+cd gap/pkg/digraphs
 make clean
 ./configure
 make
+cd ../../..
 echo "================================================================"
 echo "GAP DEV: Running tests with Semigroups loaded (but NOT Grape)..."
 echo "================================================================"
@@ -50,9 +54,11 @@ echo "GAP DEV: Running tests WITHOUT Semigroups or Grape loaded..."
 echo "============================================================"
 echo "LoadPackage(\"digraphs\"); DigraphsTestAll(); quit;" | sh gap/bin/gap.sh | tee -a testlog.txt
 
+cd gap/pkg/digraphs
 make clean
 ./configure CFLAGS=-m32 CXXFLAGS=-m32 LDFLAGS=-m32
 make
+cd ../../..
 echo "========================================================================="
 echo "GAP DEV (32-bit): Running tests with Semigroups loaded (but NOT Grape)..."
 echo "========================================================================="
