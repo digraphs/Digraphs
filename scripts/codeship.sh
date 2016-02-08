989078357778e6f92377e75da9eb89037dadc458
Michael Torpey <mct25@st-andrews.ac.uk>
2016-02-08 17:54:36 +0000

    codeship: now tests stable-4.8, release version, and 32-bit

--- scripts/codeship.sh
+++ scripts/codeship.sh
@@ -15,35 +15,8 @@ echo "GAP DEV: Running tests with Grape loaded (but NOT Semigroups)..."
 echo "================================================================"
 echo "LoadPackage(\"digraphs\"); DigraphsTestAll(); quit;" | sh gap/bin/gap.sh | tee -a testlog.txt
 
-cd gap/pkg/clone
-make clean
-if (! ./configure CFLAGS=-m32 CXXFLAGS=-m32 LDFLAGS=-m32); then echo "configure: error" >> testlog.txt; fi
-if (! make); then echo "make: error" >> testlog.txt; fi
-cd ../../..
-echo "==================================================================="
-echo "GAP DEV (32-bit): Running tests with Semigroups and Grape loaded..."
-echo "==================================================================="
-echo "(including DigraphsTestExtreme)"
-echo "-------------------------------"
-echo "LoadPackage(\"digraphs\"); LoadPackage(\"semigroups\"); DigraphsTestAll(); DigraphsTestExtreme(); quit;" | sh gap/bin/gap.sh | tee -a testlog.txt
-
-echo "==================================================================================="
-echo "GAP DEV (32-bit): Running tests with Semigroups and Grape loaded BEFORE Digraphs..."
-echo "==================================================================================="
-echo "LoadPackage(\"semigroups\"); LoadPackage(\"grape\"); LoadPackage(\"digraphs\"); DigraphsTestAll(); quit;" | sh gap/bin/gap.sh | tee -a testlog.txt
-
-echo "========================================================================="
-echo "GAP DEV (32-bit): Running tests with Grape loaded (but NOT Semigroups)..."
-echo "========================================================================="
-echo "LoadPackage(\"digraphs\"); DigraphsTestAll(); quit;" | sh gap/bin/gap.sh | tee -a testlog.txt
-
 rm -r gap/pkg/grape
 
-cd gap/pkg/clone
-make clean
-if (! ./configure); then echo "configure: error" >> testlog.txt; fi
-if (! make); then echo "make: error" >> testlog.txt; fi
-cd ../../..
 echo "================================================================"
 echo "GAP DEV: Running tests with Semigroups loaded (but NOT Grape)..."
 echo "================================================================"
@@ -54,19 +27,70 @@ echo "GAP DEV: Running tests WITHOUT Semigroups or Grape loaded..."
 echo "============================================================"
 echo "LoadPackage(\"digraphs\"); DigraphsTestAll(); quit;" | sh gap/bin/gap.sh | tee -a testlog.txt
 
-cd gap/pkg/clone
-make clean
-if (! ./configure CFLAGS=-m32 CXXFLAGS=-m32 LDFLAGS=-m32); then echo "configure: error" >> testlog.txt; fi
-if (! make); then echo "make: error" >> testlog.txt; fi
-cd ../../..
-echo "========================================================================="
-echo "GAP DEV (32-bit): Running tests with Semigroups loaded (but NOT Grape)..."
-echo "========================================================================="
-echo "LoadPackage(\"digraphs\"); LoadPackage(\"semigroups\"); DigraphsTestAll(); quit;" | sh gap/bin/gap.sh | tee -a testlog.txt
 
-echo "====================================================================="
-echo "GAP DEV (32-bit): Running tests WITHOUT Semigroups or Grape loaded..."
-echo "====================================================================="
-echo "LoadPackage(\"digraphs\"); DigraphsTestAll(); quit;" | sh gap/bin/gap.sh | tee -a testlog.txt
 
-( ! grep -E "########> Diff|brk>|#E|Error|error" testlog.txt )
+
+
+echo "=========================================================="
+echo "GAP 4.8: Running tests with Semigroups and Grape loaded..."
+echo "=========================================================="
+echo "(including DigraphsTestExtreme)"
+echo "-------------------------------"
+echo "LoadPackage(\"digraphs\"); LoadPackage(\"semigroups\"); DigraphsTestAll(); DigraphsTestExtreme(); quit;" | sh gap4r8/bin/gap.sh | tee -a testlog.txt
+
+echo "=========================================================================="
+echo "GAP 4.8: Running tests with Semigroups and Grape loaded BEFORE Digraphs..."
+echo "=========================================================================="
+echo "LoadPackage(\"semigroups\"); LoadPackage(\"grape\"); LoadPackage(\"digraphs\"); DigraphsTestAll(); quit;" | sh gap4r8/bin/gap.sh | tee -a testlog.txt
+
+echo "================================================================"
+echo "GAP 4.8: Running tests with Grape loaded (but NOT Semigroups)..."
+echo "================================================================"
+echo "LoadPackage(\"digraphs\"); DigraphsTestAll(); quit;" | sh gap4r8/bin/gap.sh | tee -a testlog.txt
+
+rm -r gap4r8/pkg/grape
+
+echo "================================================================"
+echo "GAP 4.8: Running tests with Semigroups loaded (but NOT Grape)..."
+echo "================================================================"
+echo "LoadPackage(\"digraphs\"); LoadPackage(\"semigroups\"); DigraphsTestAll(); quit;" | sh gap4r8/bin/gap.sh | tee -a testlog.txt
+
+echo "============================================================"
+echo "GAP 4.8: Running tests WITHOUT Semigroups or Grape loaded..."
+echo "============================================================"
+echo "LoadPackage(\"digraphs\"); DigraphsTestAll(); quit;" | sh gap4r8/bin/gap.sh | tee -a testlog.txt
+
+
+
+
+
+echo "=========================================================="
+echo "GAP 32b: Running tests with Semigroups and Grape loaded..."
+echo "=========================================================="
+echo "(including DigraphsTestExtreme)"
+echo "-------------------------------"
+echo "LoadPackage(\"digraphs\"); LoadPackage(\"semigroups\"); DigraphsTestAll(); DigraphsTestExtreme(); quit;" | sh gap32/bin/gap.sh | tee -a testlog.txt
+
+echo "=========================================================================="
+echo "GAP 32b: Running tests with Semigroups and Grape loaded BEFORE Digraphs..."
+echo "=========================================================================="
+echo "LoadPackage(\"semigroups\"); LoadPackage(\"grape\"); LoadPackage(\"digraphs\"); DigraphsTestAll(); quit;" | sh gap32/bin/gap.sh | tee -a testlog.txt
+
+echo "================================================================"
+echo "GAP 32b: Running tests with Grape loaded (but NOT Semigroups)..."
+echo "================================================================"
+echo "LoadPackage(\"digraphs\"); DigraphsTestAll(); quit;" | sh gap32/bin/gap.sh | tee -a testlog.txt
+
+rm -r gap32/pkg/grape
+
+echo "================================================================"
+echo "GAP 32b: Running tests with Semigroups loaded (but NOT Grape)..."
+echo "================================================================"
+echo "LoadPackage(\"digraphs\"); LoadPackage(\"semigroups\"); DigraphsTestAll(); quit;" | sh gap32/bin/gap.sh | tee -a testlog.txt
+
+echo "============================================================"
+echo "GAP 32b: Running tests WITHOUT Semigroups or Grape loaded..."
+echo "============================================================"
+echo "LoadPackage(\"digraphs\"); DigraphsTestAll(); quit;" | sh gap32/bin/gap.sh | tee -a testlog.txt
+
+( ! grep -E "########> Diff|brk>|#E|Error" testlog.txt )
