994b5d887fc96e384a62a04e4813f371f4a6433f
Wilf Wilson <wilf@wilf-wilson.net>
2018-08-28 16:01:37 +0200

    travis: fix issue with cURL

--- scripts/travis-build-dependencies.sh
+++ scripts/travis-build-dependencies.sh
@@ -60,13 +60,14 @@ for PKG in "${PKGS[@]}"; do
   cd $GAPROOT/pkg
   if [ "$PACKAGES" == "newest" ] || [ "$PKG" == "profiling" ]; then
     echo -e "\nGetting latest release of $PKG..."
-    VERSION=`curl -s -L https://github.com/gap-packages/$PKG/releases/latest | grep \<title\> | awk -F' ' '{print $2}'`
+    VERSION=`curl -sL "https://github.com/gap-packages/$PKG/releases/latest" | grep \<title\>Release | awk -F' ' '{print $2}'`
   else
     echo -e "\nGetting required release of $PKG..."
     VERSION=`grep "\"$PKG\"" $GAPROOT/pkg/digraphs/PackageInfo.g | awk -F'"' '{print $4}' | cut -c3-`
   fi
-  echo -e "Downloading $PKG-$VERSION..."
-  curl -L -O https://github.com/gap-packages/$PKG/releases/download/v$VERSION/$PKG-$VERSION.tar.gz
+  URL="https://github.com/gap-packages/$PKG/releases/download/v$VERSION/$PKG-$VERSION.tar.gz"
+  echo -e "Downloading $PKG-$VERSION, from URL:\n$URL"
+  curl -LO "$URL"
   tar xf $PKG-$VERSION.tar.gz
   rm $PKG-$VERSION.tar.gz
   cd $PKG-$VERSION
@@ -96,7 +97,7 @@ PKGS=( $GRAPE )
 for PKG in "${PKGS[@]}"; do
   echo -e "\nDownloading $PKG..."
   cd $GAPROOT/pkg
-  curl -O https://www.gap-system.org/pub/gap/gap4/tar.gz/packages/$PKG.tar.gz
+  curl -O "https://www.gap-system.org/pub/gap/gap4/tar.gz/packages/$PKG.tar.gz"
   PKG_DIR=`tar -tf $PKG.tar.gz | head -1 | cut -f1 -d"/"`
   tar xf $PKG.tar.gz
   rm $PKG.tar.gz
@@ -110,6 +111,7 @@ done
 ################################################################################
 # Install required GAP packages
 cd $GAPROOT/pkg
-curl -L -O http://www.gap-system.org/pub/gap/gap4pkgs/packages-required-master.tar.gz
+echo -e "\nGetting the required GAP packages (smallgrp, transgrp, primgrp)..."
+curl -LO "http://www.gap-system.org/pub/gap/gap4pkgs/packages-required-master.tar.gz"
 tar xf packages-required-master.tar.gz
 rm packages-required-master.tar.gz
