48722af4272ad81a67d04aab0070a867e8efbb4c
Wilf Wilson <wilf@wilf-wilson.net>
2018-09-11 17:56:01 +0200

    travis: replace stable-4.9 with stable-4.10

--- scripts/travis-test.sh
+++ scripts/travis-test.sh
@@ -2,51 +2,50 @@
 set -e
 set -o pipefail
 
-# Create the testlog and remember its location
+# Remember some important locations
 cd $HOME
 touch testlog.txt
 TESTLOG="`pwd`/testlog.txt"
+GAP_DIR="$HOME/gap"
+DIG_DIR="$GAP_DIR/pkg/digraphs"
+GAP="$GAP_DIR/bin/gap.sh"
 
 if [ "$SUITE" == "lint" ]; then
   
-  cd $HOME/gap/pkg/digraphs
+  echo -e "\nLinting with gaplint and cpplint..."
+  cd $DIG_DIR
+  gaplint `grep "^\s\+gaplint" Makefile.am | cut -d " " -f2-`
+  cpplint --extensions=c,cc,h `grep "^\s\+cpplint" Makefile.am | cut -d " " -f2-`
 
-  for FILE in `grep "^\s\+gaplint" Makefile.am | cut -d " " -f2-`; do
-    gaplint $FILE
+elif [ "$SUITE" == "coverage" ]; then
+
+  echo -e "\nPerforming code coverage tests..."
+  cd $DIG_DIR
+  for TEST in tst/standard/*.tst; do
+    FILENAME=${TEST##*/}
+    if [ ! `grep -E "$FILENAME" .covignore` ]; then
+      scripts/travis-coverage.py $TEST $THRESHOLD | tee -a $TESTLOG
+    else
+      echo -e "\033[35mignoring $FILENAME, which is listed in .covignore\033[0m"
+    fi
   done
-  for FILE in `grep "^\s\+cpplint" Makefile.am | cut -d " " -f2-`; do
-    cpplint --extensions=c,cc,h $FILE
-  done
+elif [ "$SUITE" == "test" ]; then
 
-elif [ ! -z "$GAP" ]; then
+  cd $DIG_DIR/tst/workspaces
+  echo -e "\nRunning SaveWorkspace tests..."
+  echo "LoadPackage(\"digraphs\"); DigraphsTestInstall(); Test(\"save-workspace.tst\"); quit; quit; quit;" |
+    $GAP -A -r -m 1g -T 2>&1 | tee -a $TESTLOG
+  echo -e "\nRunning LoadWorkspace tests..."
+  echo "Test(\"load-workspace.tst\"); DigraphsTestInstall(); quit; quit; quit;" |
+    $GAP -L test-output.w -A -x 80 -r -m 1g -T 2>&1 | tee -a $TESTLOG
 
-  cd $HOME/gap
-  GAP_DIR=`pwd`
-  cd $GAP_DIR/pkg/digraphs
-  if [ "$SUITE" == "coverage" ]; then
+  echo -e "\nRunning Digraphs package tests and manual examples..."
+  echo "LoadPackage(\"digraphs\"); DigraphsTestAll(); DigraphsTestExtreme();" |
+    $GAP -A -x 80 -r -m 1g -T 2>&1 | tee -a $TESTLOG
 
-    echo -e "\nPerforming code coverage tests..."
-    for TESTFILE in tst/standard/*.tst; do
-      FILENAME=${TESTFILE##*/}
-      if [ ! `grep -E "$FILENAME" .covignore` ]; then
-        ../digraphs/scripts/travis-coverage.py $TESTFILE $THRESHOLD | tee -a $TESTLOG
-      else
-        echo -e "\033[35mignoring $FILENAME, since it is listed in .covignore\033[0m"
-      fi
-    done
-
-  else
-    # Run the SaveWorkspace tests
-    cd tst/workspaces
-    echo -e "\nRunning SaveWorkspace tests..."
-    echo "LoadPackage(\"digraphs\", false); DigraphsTestInstall(); Test(\"save-workspace.tst\"); quit; quit; quit;" | $GAP_DIR/bin/gap.sh -A -r -m 1g -T 2>&1 | tee -a $TESTLOG
-    echo "Test(\"load-workspace.tst\"); DigraphsTestInstall(); quit; quit; quit;" | $GAP_DIR/bin/gap.sh -L test-output.w -A -r -m 1g -T 2>&1 | tee -a $TESTLOG
-    rm test-output.w
-    cd ../..
-    # Run all tests and manual examples
-    echo -e "\nRunning tests and manual examples..."
-    echo "LoadPackage(\"digraphs\"); DigraphsTestAll(); DigraphsTestExtreme(); quit; quit; quit;" | $GAP_DIR/bin/gap.sh -A -r -m 1g -T 2>&1 | tee $TESTLOG
-  fi
+else
+  echo -e "\nUnrecognised test suite"
+  exit 1
 fi
 
-( ! grep -E "Diff|brk>|#E|Error|error|# WARNING|fail|Syntax warning|Couldn't open saved workspace|insufficient|WARNING in|FAILED|Total errors found:" $TESTLOG )
+( ! grep -E "Diff|brk>|#E|Error|Errors detected|# WARNING|fail|Syntax warning|Couldn't open saved workspace|insufficient|WARNING in|FAILED|Total errors found:" $TESTLOG )
