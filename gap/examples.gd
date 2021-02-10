757524cd3da5391b19d29683f9a3571ace5d871c
Finn Buck <finneganlbuck@gmail.com>
2021-02-10 12:52:26 +0100

    Declare constructor functions for King's Graph.

--- gap/examples.gd
+++ gap/examples.gd
@@ -48,3 +48,7 @@ DeclareOperation("PetersenGraph", [IsFunction]);
 DeclareConstructor("GeneralisedPetersenGraphCons", [IsDigraph, IsInt, IsInt]);
 DeclareOperation("GeneralisedPetersenGraph", [IsInt, IsInt]);
 DeclareOperation("GeneralisedPetersenGraph", [IsFunction, IsInt, IsInt]);
+
+DeclareConstructor("KingsGraphCons", [IsDigraph, IsInt, IsInt]);
+DeclareOperation("KingsGraph", [IsInt, IsInt]);
+DeclareOperation("KingsGraph", [IsFunction, IsInt, IsInt]);
