c64d54fab490a18e9c83fbf1390fd9f039ce8eb5
Lea Racine <racine.lea@icloud.com>
2021-05-26 13:23:43 +0100

    fixed c linting

--- src/dfs.c
+++ src/dfs.c
@@ -22,7 +22,6 @@
 
 Obj ExecuteDFS(Obj self, Obj args) {
   DIGRAPHS_ASSERT(LEN_PLIST(args) == 7);
-  
   Obj record = ELM_PLIST(args, 1);
   Obj data  = ELM_PLIST(args, 2);
   Obj start = ELM_PLIST(args, 3);
