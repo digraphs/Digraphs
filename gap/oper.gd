11a3bfbf2dc5c082181f8b8ad96a5f10c0ff8605
Wilf Wilson <wilf@wilf-wilson.net>
2021-10-27 12:59:43 +0100

    Revert PR #459: ExecuteDFS (generic depth first search)
    In particular, this pretty much reverses commit:
    d9f060c8f9c660d7059655ff08668d1addc35489

--- gap/oper.gd
+++ gap/oper.gd
@@ -142,8 +142,3 @@ DeclareOperation("PartialOrderDigraphJoinOfVertices",
                  [IsDigraph, IsPosInt, IsPosInt]);
 DeclareOperation("PartialOrderDigraphMeetOfVertices",
                  [IsDigraph, IsPosInt, IsPosInt]);
-
-# 11. DFS
-DeclareOperation("NewDFSRecord", [IsDigraph]);
-DeclareOperation("DFSDefault", [IsRecord, IsObject]);
-DeclareGlobalFunction("ExecuteDFS");
