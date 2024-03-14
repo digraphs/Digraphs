ed875c7a9f5edf82f12ef3f24437292eef850822
Daniel Pointon <dlp14developing@gmail.com>
2024-03-14 23:10:22 +0000

    Appease linter

--- src/globals.h
+++ src/globals.h
@@ -1,7 +1,6 @@
-#ifndef cliques_maxverts
+#ifndef DIGRAPHS_SRC_GLOBALS_H_
+#define DIGRAPHS_SRC_GLOBALS_H_
 extern uint16_t cliques_maxverts;
-#endif
-#ifndef homos_maxverts
 extern uint16_t homos_maxverts;
 extern uint16_t UNDEFINED;
 #endif
