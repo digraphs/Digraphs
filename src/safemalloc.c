cd654abea4eb54ced507da9a2101f5b14cab795d
Daniel Pointon <dlp14developing@gmail.com>
2024-03-25 02:30:04 +0000

    Move import

--- src/safemalloc.c
+++ src/safemalloc.c
@@ -9,10 +9,10 @@
 **
 ********************************************************************************/
 
-#include "safemalloc.h"
-#include "digraphs-debug.h"
 #include <stdlib.h>
 
+#include "safemalloc.h"
+#include "digraphs-debug.h"
 void* safe_malloc(size_t size) {
   void* allocation = malloc(size);
   DIGRAPHS_ASSERT(allocation != NULL);
